16d6c4c7bc2099e2e96e9a25b8a7fe7a590df52b
jinheumkim <kjjppwl510@naver.com>
2023-10-24 15:19:23 +0900

    10/24 commit

--- insta/settings.py
+++ insta/settings.py
@@ -77,13 +77,19 @@ WSGI_APPLICATION = 'insta.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    
+    'default' : {
+        'ENGINE' :'mydjango.db.backends.mysql',
+        'HOST' : os.environ.get('MYSQL_HOST','localhost'),
+        'NAME' : 'clone_insta',
+        'USER' : os.environ.get('MYSQL_USER_NAME','default_user'),
+        'PASSWORD' : os.environ.get('MYSQL_PASSWORD','default_password'),
+        'PORT' : '3306',
+        'OPTIONS' : {'charset' : 'utf8mb4'}
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
